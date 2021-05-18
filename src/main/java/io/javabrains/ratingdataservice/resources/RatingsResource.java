21577e3be4f4a5b3e78af71ecad5e5fc819bd974
Aritra1704 <aritra1704@gmail.com>
2021-05-18 19:03:17 +0530

    Added Eureka client setup. Named the micro service.

--- src/main/java/io/javabrains/ratingdataservice/resources/RatingsResource.java
+++ src/main/java/io/javabrains/ratingdataservice/resources/RatingsResource.java
@@ -1,10 +1,12 @@
 package io.javabrains.ratingdataservice.resources;
 
 import io.javabrains.ratingdataservice.models.Rating;
+import io.javabrains.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,5 +19,16 @@ public class RatingsResource {
         return Collections.singletonList(
                 new Rating(movieId, 4)
         );
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRatings(ratings);
+        return userRating;
     }
 }
